fcf662dfb5d563d0fc3d03dd18bb1b60e8b44d0f
Max Hammer <132189974+Master916Max@users.noreply.github.com>
2026-09-17 14:42:28 +0200

    Fix #4

--- Kernel/IPC/ipc.py
+++ Kernel/IPC/ipc.py
@@ -4,7 +4,7 @@ from ..Core.erros import Return, ErrorType, IPCErrorCode, Error
 
 class InterProcessCommunication:
     def __init__(self):
-        self.msg_queue = []
+        self.msg_queue:list[Message] = []
 
         self.module_reg: dict[Module,list] = {Module.IPC: self.msg_queue}
 
@@ -14,3 +14,21 @@ class InterProcessCommunication:
             self.module_reg[msg.to].append(msg)
         else:
             return Return(False,Error(ErrorType.IPCError,IPCErrorCode.ModuleNotRegistered,"The Requested Module has not been Registered at the Moment"))
+
+    def handle_msgs(self) ->None:
+        for msg in self.msg_queue:
+            match msg.get_body().get("action","None"):
+                case "register":
+                    module = msg.get_body().get("module", None)
+                    if module and not module in self.module_reg:
+                        queue = msg.get_body().get("queue", None)
+                        if queue:
+                            self.module_reg[module] = queue
+                            continue
+                    msg.answer
+                        
+                    break
+                case "None":
+                    continue
+                case _ :
+                    continue
