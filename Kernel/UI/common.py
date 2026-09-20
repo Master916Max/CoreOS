7cded89bc074f6b014238b5fcf1587eab535df61
Max Hammer <132189974+Master916Max@users.noreply.github.com>
2026-09-20 08:27:33 +0200

    Add future annotations import to common.py files for improved type hinting

--- Kernel/UI/common.py
+++ Kernel/UI/common.py
@@ -1,5 +1,5 @@
+from __future__ import annotations
 from typing import  TYPE_CHECKING
-
 if TYPE_CHECKING:from .gui import GUI
 if TYPE_CHECKING:from .qadbio import QaDBIO
 if TYPE_CHECKING:from .tui import TextUserInterface
